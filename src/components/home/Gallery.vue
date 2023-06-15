1233f906024104a1102c1b7a59ebce616faa6d84
Catalina <dseguramedina@gmail.com>
2023-06-16 00:01:11 +0200

    feat: add Home page

---
+++ src/components/home/Gallery.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="gallery">
+        <div class="container">
+            <div class="card">
+                <img class="card_image" src="../../assets/logo.png" alt="">
+                <div class="card_content">
+                    <h3>Title</h3>
+                    <p>Category</p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card_image" src="../../assets/logo.png" alt="">
+                <div class="card_content">
+                    <h3>Title</h3>
+                    <p>Category</p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card_image" src="../../assets/logo.png" alt="">
+                <div class="card_content">
+                    <h3>Title</h3>
+                    <p>Category</p>
+                </div>
+            </div> 
+        </div>                    
+    </section>
+</template>
+  
+<script setup>
+
+</script>
+  
+<style lang="scss">
+@import '../../assets/sass/main.scss'
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <Gallery :msg="galleryTitle" />
+    </main>    
+</template>
+  
+<script setup>
+import { ref } from 'vue'
+import Gallery from '../components/home/Gallery.vue';
+
+const galleryTitle = ref("Cocktails")
+</script>
+  
+<style lang="scss">
+@import '../assets/sass/main.scss'
+</style>
