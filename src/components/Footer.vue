7c9f83ee8c72f4f58b73b7acfce908ac2205867e
Catalina <dseguramedina@gmail.com>
2023-06-16 00:00:18 +0200

    feat: add Header & Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="footer">
+        <p class="footer_text">© 2023 The Cocktail App. All rights reserved</p>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+@import '../assets/sass/main.scss'
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="header">
+        <img class="header_logo" src="../assets/logo.png" alt="The Cocktail App logo">
+        <h1 class="header_title">The Cocktail App</h1>  
+        <button class="btn"><i class="fa-solid fa-shuffle"></i></button>     
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+@import '../assets/sass/main.scss'
+</style>
