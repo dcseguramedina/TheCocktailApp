b4d4a96298d0db4197270a06dfed47a687e130ae
Catalina <dseguramedina@gmail.com>
2023-06-17 00:03:06 +0200

    feat: add fetch to Gallery card is not a component yet - fetch get only 1 drink

--- src/components/home/Gallery.vue
+++ src/components/home/Gallery.vue
@@ -2,31 +2,81 @@
     <section class="gallery">
         <div class="container">
             <div class="card">
-                <img class="card_image" src="../../assets/logo.png" alt="">
+                <img class="card_image" v-bind:src="drink.image" alt="">
                 <div class="card_content">
-                    <h3>Title</h3>
-                    <p>Category</p>
+                    <div class="card_title">
+                        <h3>{{ drink.title }}</h3>
+                    </div>
+                    <div class="card_description" v-if="!display_div">
+                        <p>{{drink.category}}</p>
+                        <p>Ingredients</p>
+                        <p>{{drink.instructions}}</p>
+                    </div>
                 </div>
             </div>
             <div class="card">
-                <img class="card_image" src="../../assets/logo.png" alt="">
+                <img class="card_image" src="../../assets/question.png" alt="">
                 <div class="card_content">
-                    <h3>Title</h3>
-                    <p>Category</p>
+                    <div class="card_title">
+                        <h3>Title</h3>
+                    </div>
+                    <div class="card_description" v-if="!display_div">
+                        <p>Category</p>
+                        <p>Ingredients</p>
+                        <p>Instructions</p>
+                    </div>
                 </div>
             </div>
             <div class="card">
-                <img class="card_image" src="../../assets/logo.png" alt="">
+                <img class="card_image" src="../../assets/question.png" alt="">
                 <div class="card_content">
-                    <h3>Title</h3>
-                    <p>Category</p>
+                    <div class="card_title">
+                        <h3>Title</h3>
+                    </div>
+                    <div class="card_description" v-if="!display_div">
+                        <p>Category</p>
+                        <p>Ingredients</p>
+                        <p>Instructions</p>
+                    </div>
                 </div>
             </div> 
         </div>                    
     </section>
 </template>
   
+<script>
+import Drink from '../../models/Drink';
+
+let drink = new Drink()
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        drink = new Drink(
+          data.drinks[0].idDrink, 
+          data.drinks[0].strDrinkThumb, 
+          data.drinks[0].strDrink,
+          data.drinks[0].strCategory,
+          data.drinks[0].strInstructions
+        )       
+        console.log(drink)
+      })
+      .catch((error) => {
+        alert(`Une erreur s'est produite. Veuillez réessayer`);
+      })
+
+</script>
+
 <script setup>
+//import { ref } from 'vue'
+//import Card from './Card.vue';
+
+const props = defineProps({
+  msg: String
+})
 
 </script>
   
