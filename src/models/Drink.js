c26bb557c352e44de2a0fd8637b110c79816ecd1
Catalina <dseguramedina@gmail.com>
2023-06-17 00:01:35 +0200

    feat: create Drink model

---
+++ src/models/Drink.js
@@ -0,0 +1,28 @@
+//Create lass => Drink//
+export default class Drink {
+    constructor(id, image, title, category, instructions) {
+        this.id = id;
+        this.image = image;
+        this.title = title;
+        this.category = category;
+        this.instructions = instructions
+    }
+
+    // getListOfIngredients() {
+
+    //     let listOfIngredients = [];
+
+    //     const ingredient = strIngredient;
+    //     for(i=0; i < 16; i++) {
+    //         let k = ingredient + i;
+    //         let ing = data.drinks[0].k;
+    //         if (!ing) {
+    //             break
+    //         } else {
+    //             listOfIngredients.push(ing);
+    //         }
+    //     }
+    //     console.log(listOfIngredients);
+    //     return listOfIngredients;
+    // }
+}
