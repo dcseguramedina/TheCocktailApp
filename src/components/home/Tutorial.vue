a6313e91e9c8b36c61923aa22c795aaa6f816331
Catalina <dseguramedina@gmail.com>
2023-06-16 22:05:50 +0200

    feat: add Tutorial to Home page

---
+++ src/components/home/Tutorial.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="tutorial">
+        <h2 class="tutorial_text">Let's try something new. You just need to bring your good energy and we will find some cocktails for you. If you don't like our choise, you can always go for another round and enjoy the party.</h2><br>
+        <h2 class="tutorial_text">Are you ready?</h2>
+    </div>   
+</template>
+  
+<script setup>
+// const props = defineProps({
+//   tutorialText: String, 
+// })
+</script>
+  
+<style lang="scss">
+@import '../../assets/sass/main.scss'
+</style>
