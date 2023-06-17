d83848112ef3098653189689667466e019d12a9b
Catalina <dseguramedina@gmail.com>
2023-06-17 11:23:38 +0200

    feat: add Card to Gallery

--- src/components/home/Card.vue
+++ src/components/home/Card.vue
@@ -1,11 +1,13 @@
 <template>
-    <button class="btn" @click="reloadPage">I'm still thirsty</button>    
+    <div class="card_template">
+        <slot></slot>
+    </div>    
 </template>
-  
+
 <script>
 
 </script>
-  
+
 <style lang="scss">
 @import '../../assets/sass/main.scss'
 </style>
--- src/components/home/Gallery.vue
+++ src/components/home/Gallery.vue
@@ -1,63 +1,63 @@
 <template>
     <section class="gallery">
-      <div class="gallery_items">
-            <div class="card" v-for="drink in listOfDrinks" :key="drink.id">
-                <img class="card_image" :src="drink.image" alt="">
-                <div class="card_content">
-                    <div class="card_title">
-                        <h3>{{ drink.title }}</h3>
-                    </div>
-                    <div class="card_description" v-if="!display_div">
-                        <p>{{drink.category}}</p>
-                        <p>Ingredients</p>
-                        <p>{{drink.instructions}}</p>
+        <div class="gallery_cards">
+            <Card>
+                <div class="card" v-for="drink in listOfDrinks" :key="drink.id">
+                    <img class="card_image" :src="drink.image" alt="">
+                    <div class="card_content">
+                        <div class="card_title">
+                            <h3>{{ drink.title }}</h3>
+                        </div>
+                        <div class="card_description" v-if="!display_div">
+                            <p>{{ drink.category }}</p>
+                            <p>Ingredients</p>
+                            <p>{{ drink.instructions }}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Card>
         </div>
-        <ReloadButton :msg="reloadMessage" />
+        <ReloadButton />
     </section>
 </template>
   
 <script>
+import Card from '../home/Card.vue';
 import Drink from '../../models/Drink';
-export default {
-  data() {
-    return {
-        listOfDrinks: []
-    }
-  },
-  methods: {
-    getDrinks() {
-      for(let i=0; i < 3; i++) {
-        fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-        .then(response => response.json())
-        .then(data => {
-            this.listOfDrinks.push(
-                new Drink(
-                    data.drinks[0].idDrink, 
-                    data.drinks[0].strDrinkThumb, 
-                    data.drinks[0].strDrink,
-                    data.drinks[0].strCategory,
-                    data.drinks[0].strInstructions
-                )
-            )         
-        })       
-      }
-    }
-  },
-  mounted() {
-    this.getDrinks()
-  }
-}
-</script>
-
-<script setup>
 import ReloadButton from '../home/ReloadButton.vue';
-import { ref } from 'vue'
 
-const reloadMessage = ref("I'm still thirsty<")
 
+export default {
+    components: { Card },
+
+    data() {
+        return {
+            listOfDrinks: []
+        }
+    },
+    methods: {
+        getDrinks() {
+            for (let i = 0; i < 3; i++) {
+                fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+                    .then(response => response.json())
+                    .then(data => {
+                        this.listOfDrinks.push(
+                            new Drink(
+                                data.drinks[0].idDrink,
+                                data.drinks[0].strDrinkThumb,
+                                data.drinks[0].strDrink,
+                                data.drinks[0].strCategory,
+                                data.drinks[0].strInstructions
+                            )
+                        )
+                    })
+            }
+        }
+    },
+    mounted() {
+        this.getDrinks()
+    }
+}
 </script>
   
 <style lang="scss">
